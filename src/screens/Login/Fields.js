3ab8251b5b38a3c2428c3134217a532644b82f6f
Jack <momabz6@gmail.com>
2019-03-09 00:44:51 +0700

    Update Last current work Process

--- src/screens/Login/Fields.js
+++ src/screens/Login/Fields.js
@@ -2,20 +2,67 @@ import React from 'react';
 import {View} from 'react-native';
 import { TextField } from 'react-native-material-textfield';
 import { Button } from 'react-native-elements';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 class Fields extends React.Component {
   state = {
     phone: '',
-    password : ''
+    password : '',
+    loadingSubmit : false,
+    secureTextEntry : false
   };
-  loginSubmit = () => {
 
+  constructor(props) {
+    super(props);
+  }
+  setLoadingSubmit = (loadingSubmit) => {
+    this.setState({
+      loadingSubmit
+    });
+  }
+  loginSubmit = () => {
+    this.setLoadingSubmit(true);
+    // Doing something here!
+
+    /*Validate fields*/
+    // Alert
+    //
+
+    setTimeout(() => {
+      this.setLoadingSubmit(false);
+    }, 2000)
+    //
+  }
+  onAccessoryPress() {
+    this.setState(({ secureTextEntry }) => ({ secureTextEntry: !secureTextEntry }));
+  }
+  onSubmitPassword = () => {
+    this.password.blur();
+  }
+
+  renderPasswordAccessory = () => {
+    let { secureTextEntry } = this.state;
+
+    let name = secureTextEntry?
+      'visibility':
+      'visibility-off';
+
+    return (
+      <MaterialIcons
+        size={24}
+        name={name}
+        color={TextField.defaultProps.baseColor}
+        onPress={this.onAccessoryPress}
+        suppressHighlighting
+      />
+    );
   }
   render() {
     const { phone, password} = this.state;
     return (
       <View>
         <TextField
+          ref={ref => this.phone = ref}
           style={{
             // backgroundColor : 'red',
             // padding : 10
@@ -25,16 +72,24 @@ class Fields extends React.Component {
           onChangeText={ (phone) => this.setState({ phone }) }
         />
         <TextField
+          ref={ref => this.password = ref}
+          // inputContainerPadding={20}
+          inputContainerStyle={{
+            // paddingLeft : 50
+          }}
           label='Mật khẩu'
           value={password}
           onChangeText={ (e) => {
             console.log('CHECK change password');
             console.log(e.target);
           }}
+          onSubmitEditing={this.onSubmitPassword}
+          // renderAccessory={this.renderPasswordAccessory}
         />
         <Button
           // raised
           // icon={{name: 'home', size: 32}}
+          loading={this.state.loadingSubmit}
           buttonStyle={{backgroundColor: '#00b937', borderRadius: 23,
             shadowColor: "#17984D",
             shadowOpacity: 0.5,
@@ -47,6 +102,7 @@ class Fields extends React.Component {
             marginTop : 40
           }}
           textStyle={{textAlign: 'center'}}
+          onPress={this.loginSubmit}
           // containerBorderRadius={8}
           // innerBorderStyle={}
           title={'Đăng nhập ngay'}
